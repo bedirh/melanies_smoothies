8cdbea60fe45ec09f8d5885742be634194a76784
Hakan Bedir <69045170+bedirh@users.noreply.github.com>
2024-04-19 13:18:49 +0100

    Update streamlit_app.py
    Move fruityvice code under fruit chosen block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -15,7 +16,6 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -23,12 +23,12 @@ ingredients_list = st.multiselect(
     ,max_selections = 5)
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
     ingredients_string =''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
 
@@ -45,8 +45,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered! Thank you '+ name_on_order, icon="✅")
 
-# New section to display fruityvice nutrition information
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
